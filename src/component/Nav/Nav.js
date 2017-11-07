789fe45fbe74f5dd790d16b1dba5beaaf16b8209
luisf11 <luisfernandop1110@gmail.com>
2017-11-07 09:31:32 -0400

    Add search box

--- src/component/Nav/Nav.js
+++ src/component/Nav/Nav.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import SideBar from "../SideBar/SideBar";
+import Message from '../Message/Message'
 
 class Nav extends Component {
   constructor(props) {
@@ -33,112 +34,9 @@ class Nav extends Component {
             </li>
           </ul>
           <ul className="nav navbar-nav navbar-right">
-          <li className="dropdown messages-menu">
-          <a href="#" className="dropdown-toggle" data-toggle="dropdown">
-            <i className="fa fa-envelope-o" />
-            <span className="label label-success">4</span>
-          </a>
-          <ul className="dropdown-menu">
-            <li className="header">You have 4 messages</li>
-            <li>
-              <ul className="menu">
-                <li>
-                  <a href="#">
-                    <div className="pull-left">
-                      <img
-                        src="dist/img/user2-160x160.jpg"
-                        className="img-circle"
-                        alt="User Image"
-                      />
-                    </div>
-                    <h4>
-                      Support Team
-                      <small>
-                        <i className="fa fa-clock-o" /> 5 mins
-                      </small>
-                    </h4>
-                    <p>Why not buy a new awesome theme?</p>
-                  </a>
-                </li>
-                <li>
-                  <a href="#">
-                    <div className="pull-left">
-                      <img
-                        src="dist/img/user3-128x128.jpg"
-                        className="img-circle"
-                        alt="User Image"
-                      />
-                    </div>
-                    <h4>
-                      AdminLTE Design Team
-                      <small>
-                        <i className="fa fa-clock-o" /> 2 hours
-                      </small>
-                    </h4>
-                    <p>Why not buy a new awesome theme?</p>
-                  </a>
-                </li>
-                <li>
-                  <a href="#">
-                    <div className="pull-left">
-                      <img
-                        src="dist/img/user4-128x128.jpg"
-                        className="img-circle"
-                        alt="User Image"
-                      />
-                    </div>
-                    <h4>
-                      Developers
-                      <small>
-                        <i className="fa fa-clock-o" /> Today
-                      </small>
-                    </h4>
-                    <p>Why not buy a new awesome theme?</p>
-                  </a>
-                </li>
-                <li>
-                  <a href="#">
-                    <div className="pull-left">
-                      <img
-                        src="dist/img/user3-128x128.jpg"
-                        className="img-circle"
-                        alt="User Image"
-                      />
-                    </div>
-                    <h4>
-                      Sales Department
-                      <small>
-                        <i className="fa fa-clock-o" /> Yesterday
-                      </small>
-                    </h4>
-                    <p>Why not buy a new awesome theme?</p>
-                  </a>
-                </li>
-                <li>
-                  <a href="#">
-                    <div className="pull-left">
-                      <img
-                        src="dist/img/user4-128x128.jpg"
-                        className="img-circle"
-                        alt="User Image"
-                      />
-                    </div>
-                    <h4>
-                      Reviewers
-                      <small>
-                        <i className="fa fa-clock-o" /> 2 days
-                      </small>
-                    </h4>
-                    <p>Why not buy a new awesome theme?</p>
-                  </a>
-                </li>
-              </ul>
-            </li>
-            <li className="footer">
-              <a href="#">See All Messages</a>
-            </li>
-          </ul>
-          </li>
+         
+            <Message />
+      
             <li>
               <a href="#">
                 <span className="glyphicon glyphicon-log-in" /> Login
--- src/modules/Owner/Owner.js
+++ src/modules/Owner/Owner.js
@@ -1,19 +1,30 @@
 import React, { Component } from "react";
 import Owners from "../../config/owner";
 
-
 class Owner extends Component {
   render() {
     return (
       <section>
         <div className="container">
+          <h4>Propietarios</h4>
           <div className="row">
             <div className="col-md-12">
-              <h4>Propietarios</h4>
+              <div className="form-group pull-right" style={{ width: 300 }}>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Buscar Propietarios..."
+                />
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-12">
               <div className="table-responsive">
                 <table
                   id="mytable"
-                  className="table table-bordred table-striped">
+                  className="table table-bordred table-striped"
+                >
                   <thead>
                     <tr>
                       <th>
@@ -29,49 +40,48 @@ class Owner extends Component {
                     </tr>
                   </thead>
                   <tbody>
-                    {Owners.map(owner=>(
-
-                    <tr key={owner.id}>
-                      <td>
-                        <input type="checkbox" className="checkthis" />
-                      </td>
-                      <td>{owner.first_name}</td>
-                      <td>{owner.last_name}</td>
-                      <td>{owner.email}</td>
-                      <td>{owner.phone}</td>
-                      <td>
-                        <p
-                          data-placement="top"
-                          data-toggle="tooltip"
-                          title="Edit"
-                        >
-                          <button
-                            className="btn btn-primary btn-xs"
-                            data-title="Edit"
-                            data-toggle="modal"
-                            data-target="#edit"
+                    {Owners.map(owner => (
+                      <tr key={owner.id}>
+                        <td>
+                          <input type="checkbox" className="checkthis" />
+                        </td>
+                        <td>{owner.first_name}</td>
+                        <td>{owner.last_name}</td>
+                        <td>{owner.email}</td>
+                        <td>{owner.phone}</td>
+                        <td>
+                          <p
+                            data-placement="top"
+                            data-toggle="tooltip"
+                            title="Edit"
                           >
-                            <span className="glyphicon glyphicon-pencil" />
-                          </button>
-                        </p>
-                      </td>
-                      <td>
-                        <p
-                          data-placement="top"
-                          data-toggle="tooltip"
-                          title="Delete"
-                        >
-                          <button
-                            className="btn btn-danger btn-xs"
-                            data-title="Delete"
-                            data-toggle="modal"
-                            data-target="#delete"
+                            <button
+                              className="btn btn-primary btn-xs"
+                              data-title="Edit"
+                              data-toggle="modal"
+                              data-target="#edit"
+                            >
+                              <span className="glyphicon glyphicon-pencil" />
+                            </button>
+                          </p>
+                        </td>
+                        <td>
+                          <p
+                            data-placement="top"
+                            data-toggle="tooltip"
+                            title="Delete"
                           >
-                            <span className="glyphicon glyphicon-trash" />
-                          </button>
-                        </p>
-                      </td>
-                    </tr>
+                            <button
+                              className="btn btn-danger btn-xs"
+                              data-title="Delete"
+                              data-toggle="modal"
+                              data-target="#delete"
+                            >
+                              <span className="glyphicon glyphicon-trash" />
+                            </button>
+                          </p>
+                        </td>
+                      </tr>
                     ))}
                   </tbody>
                 </table>
