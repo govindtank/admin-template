481bb6cea38ed8399d409bafff7680f797fe0b93
luisf11 <luisfernandop1110@gmail.com>
2017-12-14 16:53:00 -0400

    Add select on  data table

--- src/modules/Owner/Owner.js
+++ src/modules/Owner/Owner.js
@@ -4,13 +4,39 @@ import Modal from "../../component/Modal/Modal";
 import Pagination from "../../component/Pagination/Pagination";
 import { BootstrapTable, TableHeaderColumn } from "react-bootstrap-table";
 
-
-
 class Owner extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selected: [],
+      currPage: 1
+    };
+  }
   render() {
+    const { currPage } = this.state;
+    const selectRowProp = {
+      mode: "checkbox",
+      bgColor: "pink",
+      clickToSelect: true
+    };
+    const options = {
+      sizePerPageList: [5, 10, 15, 20],
+      sizePerPage: 10,
+      page: currPage,
+      sortName: "id",
+      sortOrder: "asc"
+    };
     return (
       <div>
-        <BootstrapTable data={Owners} striped hover>
+        <h3>Propietarios</h3>
+        <BootstrapTable
+          data={Owners}
+          selectRow={selectRowProp}
+          striped
+          hover
+          pagination={true}
+          options={options}
+        >
           <TableHeaderColumn isKey dataField="id">
             Product ID
           </TableHeaderColumn>
